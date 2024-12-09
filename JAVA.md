6ee88b1a8dce58fca9003025cf9c0fbd42a5e489
Michael L Perry <michael@qedcode.com>
2024-12-08 18:47:33 -0600

    Produce a message

--- JAVA.md
+++ JAVA.md
@@ -9,12 +9,26 @@ sudo apt install maven
 sudo apt install gradle
 ```
 
+On MacOS
+
+```bash
+brew update
+brew upgrade
+brew install openjdk@21
+brew install maven
+brew install gradle
+```
+
 ```bash
 $ java --version
 openjdk 21.0.5 2024-10-15
 OpenJDK Runtime Environment (build 21.0.5+11-Ubuntu-1ubuntu120.04)
 OpenJDK 64-Bit Server VM (build 21.0.5+11-Ubuntu-1ubuntu120.04, mixed mode, sharing)
 ```
+
+## Visual Studio Code
+
+Install the [Java Extension Pack](https://marketplace.visualstudio.com/items?itemName=vscjava.vscode-java-pack).
 
 ## Build and Run Hello World
 
--- first-producer/src/main/java/com/qedcode/App.java
+++ first-producer/src/main/java/com/qedcode/App.java
@@ -1,10 +1,28 @@
 package com.qedcode;
 
+import java.util.Properties;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
 /**
- * Hello world!
+ * Your first Kafka producer
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Properties kafkaProps = new Properties();
+        kafkaProps.put("bootstrap.servers", "localhost:9092");
+        kafkaProps.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        kafkaProps.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<String, String> producer = new KafkaProducer<>(kafkaProps);
+        ProducerRecord<String, String> record = new ProducerRecord<>("CustomerCountry", "Precision Products", "France");
+        try {
+            producer.send(record);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            producer.close();
+        }
     }
 }

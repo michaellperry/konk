5c759f92c46a76c530cc528d3ab6c3d8e6913bf3
Michael L Perry <michael@qedcode.com>
2024-12-08 20:38:00 -0600

    Add the necessary dependencies to the new project

--- generate_project.sh
+++ generate_project.sh
@@ -10,3 +10,48 @@ PROJECT_NAME=$1
 
 # Execute the Maven archetype generate command with the provided project name
 mvn archetype:generate -DgroupId=com.qedcode -DartifactId=$PROJECT_NAME -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.5 -DinteractiveMode=false
+
+# Add Kafka, Logback, and SLF4J dependencies to the generated pom.xml
+POM_FILE="$PROJECT_NAME/pom.xml"
+sed -i '' '/<\/dependencies>/ i\
+    <dependency>\
+      <groupId>org.apache.kafka</groupId>\
+      <artifactId>kafka-clients</artifactId>\
+      <version>3.0.0</version>\
+    </dependency>\
+    <dependency>\
+      <groupId>org.slf4j</groupId>\
+      <artifactId>slf4j-api</artifactId>\
+      <version>1.7.32</version>\
+    </dependency>\
+    <dependency>\
+      <groupId>ch.qos.logback</groupId>\
+      <artifactId>logback-classic</artifactId>\
+      <version>1.2.6</version>\
+    </dependency>' $POM_FILE
+
+# Create resources directory and add kafka.properties and logback.xml files
+mkdir -p "$PROJECT_NAME/src/main/resources"
+
+# Add kafka.properties file
+cat <<EOL > "$PROJECT_NAME/src/main/resources/kafka.properties"
+bootstrap.servers=localhost:9092,localhost:9093,localhost:9094
+key.serializer=org.apache.kafka.common.serialization.StringSerializer
+value.serializer=org.apache.kafka.common.serialization.StringSerializer
+acks=all
+EOL
+
+# Add logback.xml file
+cat <<EOL > "$PROJECT_NAME/src/main/resources/logback.xml"
+<configuration>
+    <appender name="STDOUT" class="ch.qos.logback.core.ConsoleAppender">
+        <encoder>
+            <pattern>%d{yyyy-MM-dd HH:mm:ss} %-5level %logger{36} - %msg%n</pattern>
+        </encoder>
+    </appender>
+
+    <root level="INFO">
+        <appender-ref ref="STDOUT" />
+    </root>
+</configuration>
+EOL

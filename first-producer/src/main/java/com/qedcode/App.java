0722c96cb495253083305e0ea8386e46a0155e95
Michael L Perry <michael@qedcode.com>
2024-12-08 18:51:23 -0600

    Add SLF4J and Logback dependencies for logging in Kafka producer

--- first-producer/src/main/java/com/qedcode/App.java
+++ first-producer/src/main/java/com/qedcode/App.java
@@ -4,11 +4,15 @@ import java.util.Properties;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Your first Kafka producer
  */
 public class App {
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args) {
         Properties kafkaProps = new Properties();
         kafkaProps.put("bootstrap.servers", "localhost:9092");
@@ -19,8 +23,9 @@ public class App {
         ProducerRecord<String, String> record = new ProducerRecord<>("CustomerCountry", "Precision Products", "France");
         try {
             producer.send(record);
+            logger.info("Message sent successfully");
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error("Error sending message", e);
         } finally {
             producer.close();
         }
